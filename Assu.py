6bdb2f41692eea41e8e6f9d799a0c16d031308fc
yashchandani98 <yashchandani98.yc@gmail.com>
2019-07-28 20:29:37 +0530

    Initial Commit

--- Assu.py
+++ Assu.py
@@ -3,7 +3,6 @@ import speech_recognition as speech
 import wikipedia
 import datetime
 import os
-import pyaudio
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[1].id)
