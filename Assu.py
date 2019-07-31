01fe095b166be58ad991ec13472d48d7a1d785d6
yashchandani98 <yashchandani98.yc@gmail.com>
2019-08-01 01:12:05 +0530

    Added some functionalities

--- Assu.py
+++ Assu.py
@@ -3,10 +3,12 @@ import speech_recognition as speech
 import wikipedia
 import datetime
 import os
+import webbrowser
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[1].id)
 wishes=['good morning','good evening','good afternoon']
+websites = {'youtube': 'youtube.com','facebook':'facebook.com','google':'google.com','gmail':'gmail.com'}
 def speak(audio):
     engine.say(audio)
     engine.runAndWait()
@@ -14,15 +16,17 @@ def initial():
     hour = int(datetime.datetime.now().hour)
     if hour>=0 and hour<12:
         speak("Good Morning Buddy!")
+        print("Assu:Good Morning Buddy!")
     elif hour>=12 and hour<18:
         speak("Good Afternoon Buddy!")
+        print("Assu:Good Afternoon Buddy!")
     else:
         speak("Good Evening Buddy!")
+        print("Assu:Good Evening Buddy!")
 def listen():
     r = speech.Recognizer()
     with speech.Microphone() as src:
         print("Listening...")
-        r.pause_threshold=2
         audio=r.listen(src)
     try:
         print("Recognizing...")
@@ -36,6 +40,34 @@ if __name__ == "__main__":
     initial()
     speak("My name is Assu. How can I help you?")
     while True:
-        string = listen().lower()
-        if string in wishes:
-            initial()
+        try:
+            string = listen().lower()
+            if string in wishes:
+                initial()
+            elif "what is your name" in string:
+                speak("My name is Assu.")
+                print("Assu:My name is Assu.")
+            elif "wikipedia" in string:
+                speak("Collecting information from Wikipedia")
+                summ=wikipedia.summary("New York")
+                speak("According to Wiipedia:")
+                print(summ)
+                speak(summ)
+            elif "open" in string:
+                for key in websites:
+                    if key in string:
+                        webbrowser.open(websites[key])
+            elif "time" in string:
+                time=datetime.datetime.now().strftime("%H:%M:%S")
+                speak(f"The time is {time}")
+                print(f"Assu:The time is {time}")
+            elif "date" in string:
+                date=datetime.datetime.now().strftime("%d:%B%Y")
+                speak(f"The date is {date}")
+                print(f"Assu:The date is {date}")
+            elif "today" in string:
+                day = datetime.datetime.now().strftime("%A")
+                speak(f"Today is {day}")
+                print(f"Assu:Today is {day}")
+        except Exception as e:
+            print(e)
